d42d9eaa774fde185e7bc89551aecec3a8d1b15b
patoliyabhi7 <patoliyabhi7@gmail.com>
2024-09-02 16:31:24 +0530

    .

--- utils/email.js
+++ utils/email.js
@@ -1,62 +1,16 @@
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (options) => {
-//     try {
-//         const transporter = nodemailer.createTransport({
-//             service: 'Gmail',
-//             auth: {
-//                 user: process.env.EMAIL_USERNAME,
-//                 pass: process.env.EMAIL_PASSWORD
-//             }
-//         });
-
-//         const mailOptions = {
-//             to: options.email,
-//             subject: options.subject,
-//             text: options.message,
-//         };
-
-//         // Conditionally add attachments
-//         if (options.fileName && options.filePath) {
-//             mailOptions.attachments = [{
-//                 filename: options.fileName,
-//                 path: options.filePath
-//             }];
-//         }
-
-//         // Send the email
-//         await transporter.sendMail(mailOptions);
-//     } catch (error) {
-//         console.error("Error sending email:", error);
-//         throw error;
-//     }
-// }
-
-// module.exports = sendEmail;
-
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     try {
         const transporter = nodemailer.createTransport({
-            name: 'movya.com',
-            host: 'shared67.accountservergroup.com',
-            port: 465,
-            secure: true,
+            service: 'Gmail',
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD
             }
         });
 
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-            }
-        });
-
         const mailOptions = {
-            from: `Abhi Movya <${process.env.EMAIL_USERNAME}>`,
             to: options.email,
             subject: options.subject,
             text: options.message,
@@ -70,6 +24,7 @@ const sendEmail = async (options) => {
             }];
         }
 
+        // Send the email
         await transporter.sendMail(mailOptions);
     } catch (error) {
         console.error("Error sending email:", error);
