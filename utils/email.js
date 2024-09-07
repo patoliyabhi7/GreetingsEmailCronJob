71ab92ece60ce9c4801eaf422e48cd35630038ae
patoliyabhi7 <patoliyabhi7@gmail.com>
2024-09-07 13:05:05 +0530

    .

--- utils/email.js
+++ utils/email.js
@@ -1,39 +1,3 @@
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (options) => {
-//     try {
-//         const transporter = nodemailer.createTransport({
-//             service: 'Gmail',
-//             auth: {
-//                 user: process.env.EMAIL_USERNAME,
-//                 pass: process.env.EMAIL_PASSWORD
-//             }
-//         });
-
-//         const mailOptions = {
-//             to: options.email,
-//             subject: options.subject,
-//             text: options.message,
-//         };
-
-//         // Conditionally add attachments
-//         if (options.fileName && options.filePath) {
-//             mailOptions.attachments = [{
-//                 filename: options.fileName,
-//                 path: options.filePath
-//             }];
-//         }
-
-//         // Send the email
-//         await transporter.sendMail(mailOptions);
-//     } catch (error) {
-//         console.error("Error sending email:", error);
-//         throw error;
-//     }
-// }
-
-// module.exports = sendEmail;
-
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
@@ -56,7 +20,7 @@ const sendEmail = async (options) => {
         });
 
         const mailOptions = {
-            from: `Abhi Movya <${process.env.EMAIL_USERNAME}>`,
+            from: `Movya Infotech <${process.env.EMAIL_USERNAME}>`,
             to: options.email,
             subject: options.subject,
             text: options.message,
