ff99c91f67b758eaed05868e2d533cf75f41d2ff
patoliyabhi7 <patoliyabhi7@gmail.com>
2024-10-26 16:53:59 +0530

    updated host to hostinger

--- utils/email.js
+++ utils/email.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'shared67.accountservergroup.com',
-            port: 587,
+            host: 'smtp.hostinger.com',
+            port: 465,
             secure: false,
             auth: {
                 user: process.env.EMAIL_USERNAME,
